72d5bab5e8be08aee1b695c5371f143df0a93a98
juliengauthier410 <juliengauthier410@penguin>
2023-05-27 21:45:28 +0200

    creat void send_delete(argv)

--- taskcli.c
+++ taskcli.c
@@ -195,59 +195,76 @@ void read_tasks() {
 }
 
 
+void send_delete(argv) {
+    //on vérifie que argv[2] est bien un nombre
+    for (int i = 0; i < strlen(argv[2]); i++) {
+        if (!isdigit(argv[2][i])) {
+            fprintf(stderr, "Error : Invalid number of command line.\n");
+            fprintf(stderr, "Usage : ./taskcli START PERIOD CMD [ARG]...\nUsage : ./taskcli -d numCommandLine\n : ./taskcli\n");
+            exit(1);
+        }
+    }
+    // on récupère le pid de taskd
+    pid_t pid = get_taskd_pid();
+    if(pid == -1) {
+        exit(1); // the error is print from the fonction
+    }
+    // on envoie le signal SIGUSR2 au daemon
+    kill(pid, SIGUSR2);
+    // on envoie l'entier num dans le fifo
+    int fifo = open(LINK_FIFO, O_WRONLY);
+
+
+    if (fifo == -1) {
+        perror("open");
+        exit(EXIT_FAILURE);
+    }
+
+    //-------------Locking the fifo------------------
+    struct flock lock;
+    lock.l_type = F_WRLCK;        // Write lock
+    lock.l_whence = SEEK_SET;     // Offset is relative to the start of the file
+    lock.l_start = 0;             // Start offset for the lock
+    lock.l_len = 0;               // Lock the entire file; 0 means to EOF
+
+    if (fcntl(fifo, F_SETLKW, &lock) == -1) {
+        perror("fcntl");
+        close(fifo);
+        exit(EXIT_FAILURE);
+    }
+    //-----------------------------------------------
+    send_string(fifo, argv[2]);
+    //---------Unlocking the fifo--------------------
+    lock.l_type = F_UNLCK;
+    if (fcntl(fifo, F_SETLKW, &lock) == -1) {
+        perror("fcntl");
+        close(fifo);
+        exit(EXIT_FAILURE);
+    }
+    //-----------------------------------------------
+    close(fifo);
+
+}
+
+
+
+
+
 int main(int argc, char *argv[]) {
     if(argc == 1) {
         read_tasks();
         exit(0);
     }else if (argc == 3) {
         if(strcmp(argv[1], "-d") == 0) {
-            //on vérifie que argv[2] est bien un nombre
-            if (argv[2][0] < '0' || argv[2][0] > '9') {
-                fprintf(stderr, "Error : Invalid number of command line.\n");
-                fprintf(stderr, "Usage : ./taskcli START PERIOD CMD [ARG]...\nUsage : ./taskcli -d numCommandLine\n : ./taskcli\n");
-                exit(1);
-            }
-            // on récupère le pid de taskd
-            pid_t pid = get_taskd_pid();
-            if(pid == -1) {
-                exit(1); // the error is print from the fonction
-            }
-            // on envoie le signal SIGUSR2 au daemon
-            kill(pid, SIGUSR2);
-            // on envoie l'entier num dans le fifo
-            int fifo = open(LINK_FIFO, O_WRONLY);
-
-    
-            if (fifo == -1) {
-                perror("open");
-                exit(EXIT_FAILURE);
-            }
-
-            //-------------Locking the fifo------------------
-            struct flock lock;
-            lock.l_type = F_WRLCK;        // Write lock
-            lock.l_whence = SEEK_SET;     // Offset is relative to the start of the file
-            lock.l_start = 0;             // Start offset for the lock
-            lock.l_len = 0;               // Lock the entire file; 0 means to EOF
-
-            if (fcntl(fifo, F_SETLKW, &lock) == -1) {
-                perror("fcntl");
-                close(fifo);
-                exit(EXIT_FAILURE);
-            }
-            //-----------------------------------------------
-            send_string(fifo, argv[2]);
-            //---------Unlocking the fifo--------------------
-            lock.l_type = F_UNLCK;
-            if (fcntl(fifo, F_SETLKW, &lock) == -1) {
-                perror("fcntl");
-                close(fifo);
-                exit(EXIT_FAILURE);
-            }
-            //-----------------------------------------------
-            close(fifo);
-
+            send_delete(argv);
         }
+        else {
+            fprintf(stderr, "Error : Invalid number of arguments.\n");
+            fprintf(stderr, "Usage : ./taskcli START PERIOD CMD [ARG]...\nUsage : ./taskcli -d numCommandLine\n : ./taskcli\n");
+            exit(1);
+        }
+        exit(0);
+    }
 
             
 
